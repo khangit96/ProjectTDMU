0c04cba386e5360c557a506f4e8da727fc99d011
khangit96 <khangit69@gmail.com>
2016-10-21 19:36:08 +0700

    sks

--- ShopThoiTrang/ShopThoiTrang/Content/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('ul.nav li.dropdown').hover(function(){
-    	$(this).addClass('open');
-    	$('.dropdown-menu', this).fadeIn();
-    }, function(){
-    	$(this).removeClass('open');
-    	$('.dropdown-menu', this).fadeOut('fast');
-    });
-});
